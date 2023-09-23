818228b7a7a0ce2d355d29a10aaebd2b4a735acd
Gideon Karuga <karuga.gideon@gmail.com>
2023-09-23 15:39:53 +0300

    first commit

---
+++ src/components/Welcome.tsx
@@ -0,0 +1,8 @@
+export const Welcome: React.FC = () => (
+  <div className="bg-white border-gray-100 border-2 rounded-lg px-8 py-5 mr-20 w-full">
+    <h1 className="text-2xl font-bold mb-2">🤖 MaishaMoney AI</h1>
+    <p>
+      Empowering Financial Journeys, One Cent at a Time
+    </p>
+  </div>
+);
